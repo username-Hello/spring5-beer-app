d75d5dfc938727d1e69144ade669fa01d8c5ce41
nikit <nikitos.burak@gmail.com>
2020-09-17 10:59:39 +0300

    initial commit

---
+++ src/main/java/com/springframework/spring5beerapp/Spring5BeerAppApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5beerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5BeerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5BeerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/spring5beerapp/Spring5BeerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5beerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5BeerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
